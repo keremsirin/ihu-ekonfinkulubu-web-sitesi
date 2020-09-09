d583f45843b19703050d36f87ad3e191764c468b
Kerem Sirin <keremsirinis@gmail.com>
2020-09-09 21:11:30 +0300

    Footer taslağı oluşturuldu.

--- dist/main.3dfbff5f.js
+++ dist/main.3dfbff5f.js
@@ -153,7 +153,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49762" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59567" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
